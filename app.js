d9a49fb63c158d2e8b0d0edc64ffbef38182523c
Priyadharshini <pudutha@uscthinktank.lan>
2020-10-28 12:16:15 -0700

    added routes and some logic

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 /** Simple demo Express app. */
 
 const express = require("express");
+const { findMean, findMedian, findMode } = require("./stats")
+const { convertStrNums } = require("./utils")
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res, next) {
+  if (!req.query.nums) {
+    return next(new BadRequestError(message = "nums are required"));
+  }
+  let strNums = req.query.nums;
+  let intNums = convertStrNums(strNums.split(','))
+
+  if (intNums instanceof BadRequestError) return next(intNums);
+
+  let result = findMean(intNums);
+
+  return res.json({ response: { operation: "mean", value: result } });
+})
 
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res, next) {
+  if (!req.query.nums) {
+    return next(new BadRequestError(message = "nums are required"));
+  }
+  let strNums = req.query.nums;
+  let intNums = convertStrNums(strNums.split(','))
+
+  if (intNums instanceof BadRequestError) return next(intNums);
+  let result = findMedian(intNums);
+
+  return res.json({ response: { operation: "median", value: result } });
+})
 
 
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function (req, res, next) {
+  if (!req.query.nums) {
+    return next(new BadRequestError(message = "nums are required"));
+  }
+  let strNums = req.query.nums;
+  let intNums = convertStrNums(strNums.split(','))
+
+  if (intNums instanceof BadRequestError) return next(intNums);
+  let result = findMode(intNums);
+
+  return res.json({ response: { operation: "mode", value: result } });
+})
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
--- utils.js
+++ utils.js
@@ -1,11 +1,20 @@
 const { BadRequestError } = require("./expressError");
 
-
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  try {
+      result = strNums.map(el=> {
+        let intEl = parseInt(el);
+        if (!intEl) throw new BadRequestError(`${el} is not a number`);
+        return intEl;
+      })
+      return result;
+  } catch(err){
+      return err;
+  }
 }
 
 
